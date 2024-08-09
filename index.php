6742c73d1a0a477a4b082e18912e130d85855a66
Aloka_Chamod_Champathi <145195460+Alokachamod@users.noreply.github.com>
2024-08-10 01:28:18 +0530

    £

--- index.php
+++ index.php
@@ -8,10 +8,10 @@
   <link rel="stylesheet" href="style.css">
   <link rel="stylesheet" href="bootstrap.css">
   <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Share+Tech+Mono&display=swap" rel="stylesheet">
-<link rel="icon" href="resources/swexzo logo.jpg">
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bodymovin/5.7.5/lottie.min.js"></script>
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Share+Tech+Mono&display=swap" rel="stylesheet">
+  <link rel="icon" href="resources/swexzo logo.jpg">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/bodymovin/5.7.5/lottie.min.js"></script>
 </head>
 
 <body class="bg-dark">
@@ -28,39 +28,61 @@
           <div class="collapse navbar-collapse " id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0 offset-xxl-4 ">
               <li class="nav-item ">
-                <a class="nav-link position-relative g-6 m-3" aria-current="page" href="#sxhome"><p class="htext">Home</p></a>
+                <a class="nav-link position-relative g-6 m-3" aria-current="page" href="#sxhome">
+                  <p class="htext">Home</p>
+                </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link position-relative g-6 m-3" href="#"><p class="htext">Services</p></a>
+                <a class="nav-link position-relative g-6 m-3" href="#">
+                  <p class="htext">Services</p>
+                </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link position-relative g-6 m-3" href="#"><p class="htext">Projects</p></a>
+                <a class="nav-link position-relative g-6 m-3" href="#">
+                  <p class="htext">Projects</p>
+                </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link position-relative g-6 m-3" href="#"><p class="htext">Our Team</p></a>
+                <a class="nav-link position-relative g-6 m-3" href="#">
+                  <p class="htext">Our Team</p>
+                </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link position-relative g-6 m-3" href="#"><p class="htext">Contact us</p></a>
+                <a class="nav-link position-relative g-6 m-3" href="#">
+                  <p class="htext">Contact us</p>
+                </a>
               </li>
             </ul>
           </div>
         </div>
       </nav>
 
-     <section class="col-12 h-100 border-bottom-1" id="sxhome">
-        <div  class="homeanimation d-flex position-absolute offset-7">
-          <div id="lottie-animation" style="width: 600px; height: 600px;">
-    
+      <section class="col-12 h-100 border-bottom-1" id="sxhome">
+
+        <div class="d-flex h-100">
+          <div class="col-6">
+            <h5>
+            Welcome to Swexzo, 
+            </h5>
+            <p>
+              cutting-edge software company committed to delivering innovative technology solutions that empower businesses and enhance digital experiences
+            </p>
+          </div>
+          <div class="homeanimation d-flex position-absolute offset-6 col-lg-6">
+            <div id="lottie-animation" style="width: 600px; height: 600px;">
+
+            </div>
           </div>
         </div>
-     </section>
+
+      </section>
 
 
     </div>
   </div>
   </div>
   <script src="script.js"></script>
-  
+
   <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
   <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
