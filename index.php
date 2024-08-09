aa78f4f58ca5f92daedf1f0bdd62e93f50916251
Aloka_Chamod_Champathi <145195460+Alokachamod@users.noreply.github.com>
2024-08-10 01:10:56 +0530

    #

--- index.php
+++ index.php
@@ -2,51 +2,69 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Swexzo</title>
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="bootstrap.css">
-    <link href="./output.css" rel="stylesheet">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Swexzo</title>
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="bootstrap.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Share+Tech+Mono&display=swap" rel="stylesheet">
+<link rel="icon" href="resources/swexzo logo.jpg">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bodymovin/5.7.5/lottie.min.js"></script>
 </head>
 
 <body class="bg-dark">
-    <div class="container-fluid">
-        <div class="col-12">
-            <header class="navbar navbar-bright navbar-fixed-top" role="banner">
-                <div class="container">
-                    <div class="navbar-header">
-                        <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".navbar-collapse">
-                            <span class="sr-only">Toggle navigation</span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                        </button>
-                        <a href="/" class="navbar-brand">Hello</a>
-                    </div>
-                    <nav class="collapse navbar-collapse" role="navigation">
-                        <ul class="nav navbar-nav">
-                            <li>
-                                <a href="#">Get Started</a>
-                            </li>
-                            <li>
-                                <a href="#">Edit</a>
-                            </li>
-                            <li>
-                                <a href="#">Visualize</a>
-                            </li>
-                            <li>
-                                <a href="#">Prototype</a>
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
-            </header>
+  <div class="container-fluid">
+    <div class="col-12">
+      <nav class="navbar navbar-expand-lg col-10 offset-1 header">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            <img src="resources/Swexzo company logo.png" alt="icon" height="60px">
+          </a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse " id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0 offset-xxl-4 ">
+              <li class="nav-item ">
+                <a class="nav-link position-relative g-6 m-3" aria-current="page" href="#sxhome"><p class="htext">Home</p></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link position-relative g-6 m-3" href="#"><p class="htext">Services</p></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link position-relative g-6 m-3" href="#"><p class="htext">Projects</p></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link position-relative g-6 m-3" href="#"><p class="htext">Our Team</p></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link position-relative g-6 m-3" href="#"><p class="htext">Contact us</p></a>
+              </li>
+            </ul>
+          </div>
         </div>
+      </nav>
+
+     <section class="col-12 h-100 border-bottom-1" id="sxhome">
+        <div  class="homeanimation d-flex position-absolute offset-7">
+          <div id="lottie-animation" style="width: 600px; height: 600px;">
+    
+          </div>
+        </div>
+     </section>
+
+
     </div>
-    <script src="script.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+  </div>
+  </div>
+  <script src="script.js"></script>
+  
+  <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+  <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
 </body>
 
 </html>
