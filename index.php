2428992247e5fa79ab851652eafb83c9c90c0ca8
Aloka_Chamod_Champathi <145195460+Alokachamod@users.noreply.github.com>
2024-08-10 23:31:27 +0530

    #

--- index.php
+++ index.php
@@ -10,17 +10,19 @@
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Share+Tech+Mono&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Edu+AU+VIC+WA+NT+Hand:wght@400..700&family=Share+Tech+Mono&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Chakra+Petch:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Edu+AU+VIC+WA+NT+Hand:wght@400..700&family=Share+Tech+Mono&display=swap" rel="stylesheet">
   <link rel="icon" href="resources/swexzo logo.jpg">
   <script src="https://cdnjs.cloudflare.com/ajax/libs/bodymovin/5.7.5/lottie.min.js"></script>
 </head>
 
-<body class="bg-dark" style="overflow: hidden;">
+<body class="bg-dark ">
   <div class="container-fluid">
     <div class="col-12">
-      <nav class="navbar navbar-expand-lg col-10 offset-1 header">
+      <nav class="navbar navbar-expand-lg col-10 offset-1 header fixed-top">
         <div class="container-fluid">
           <a class="navbar-brand" href="#">
-            <img src="resources/Swexzo company logo.png" alt="icon" height="60px">
+            <img src="resources/Swexzo company logo.png" alt="icon" height="50px">
           </a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -33,7 +35,7 @@
                 </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link position-relative g-6 m-3" href="#">
+                <a class="nav-link position-relative g-6 m-3" href="#sxservice">
                   <p class="htext">Services</p>
                 </a>
               </li>
@@ -57,24 +59,114 @@
         </div>
       </nav>
 
-      <section class="col-12" id="sxhome">
-
-        <div class="d-flex">
-          <div class="col-6">
-            <h5>
-              Welcome to Swexzo,
-            </h5>
-            <h6>
-              cutting-edge software company committed to delivering innovative technology solutions that empower businesses and enhance digital experiences
-            </h6>
-          </div>
-          <div class="homeanimation d-flex position-absolute offset-6 col-lg-6">
-            <div id="lottie-animation" style="width: 600px; height: 600px;">
-
+      <section class="col-12 sxhome h-100" id="sxhome">
+        <div class="row">
+          <div class="d-flex">
+            <div class="col-6 text-light">
+              <div class="col-lg-10 offset-lg-1 col-11  offset-1 hometext  ">
+                <img src="resources/Swexzo company logo.png" alt="icon" class="himage offset-2">
+                <p class="slogen1 offset-1">Your partner in</p>
+                <p class="slogen2 offset-2">Digital Transformation</p>
+                
+              </div>
             </div>
+            <div class="homeanimation position-absolute offset-6 col-lg-6  d-none d-xl-block">
+                  <div id="lottie-animation" style="width: 700px; height: 700px;">
+
+                  </div>
+                </div>
           </div>
         </div>
+      </section>
 
+      <section class="col-12 sxservice " id="sxservice">
+        <div class="col-12">
+          <div class="stitle text-light text-center">Our Services</div>
+          <br>
+          <div class="container py-4">
+		<h1 class="h1 text-center" id="pageHeaderTitle">My Cards Dark</h1>
+
+		<article class="postcard dark blue">
+			<a class="postcard__img_link" href="#">
+				<img class="postcard__img" src="https://picsum.photos/1000/1000" alt="Image Title" />
+			</a>
+			<div class="postcard__text">
+				<h1 class="postcard__title blue"><a href="#">Podcast Title</a></h1>
+				<div class="postcard__subtitle small">
+					
+				</div>
+				<div class="postcard__bar"></div>
+				<div class="postcard__preview-txt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, fugiat asperiores inventore beatae accusamus odit minima enim, commodi quia, doloribus eius! Ducimus nemo accusantium maiores velit corrupti tempora reiciendis molestiae repellat vero. Eveniet ipsam adipisci illo iusto quibusdam, sunt neque nulla unde ipsum dolores nobis enim quidem excepturi, illum quos!</div>
+				<ul class="postcard__tagbox">
+					<li class="tag__item"><i class="fas fa-tag mr-2"></i>Podcast</li>
+					<li class="tag__item"><i class="fas fa-clock mr-2"></i>55 mins.</li>
+					<li class="tag__item play blue">
+						<a href="#"><i class="fas fa-play mr-2"></i>Play Episode</a>
+					</li>
+				</ul>
+			</div>
+		</article>
+		<article class="postcard dark red">
+			<a class="postcard__img_link" href="#">
+				<img class="postcard__img" src="https://picsum.photos/501/500" alt="Image Title" />	
+			</a>
+			<div class="postcard__text">
+				<h1 class="postcard__title red"><a href="#">Podcast Title</a></h1>
+				<div class="postcard__subtitle small">
+					
+				</div>
+				<div class="postcard__bar"></div>
+				<div class="postcard__preview-txt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, fugiat asperiores inventore beatae accusamus odit minima enim, commodi quia, doloribus eius! Ducimus nemo accusantium maiores velit corrupti tempora reiciendis molestiae repellat vero. Eveniet ipsam adipisci illo iusto quibusdam, sunt neque nulla unde ipsum dolores nobis enim quidem excepturi, illum quos!</div>
+				<ul class="postcard__tagbox">
+					<li class="tag__item"><i class="fas fa-tag mr-2"></i>Podcast</li>
+					<li class="tag__item"><i class="fas fa-clock mr-2"></i>55 mins.</li>
+					<li class="tag__item play red">
+						<a href="#"><i class="fas fa-play mr-2"></i>Play Episode</a>
+					</li>
+				</ul>
+			</div>
+		</article>
+		<article class="postcard dark green">
+			<a class="postcard__img_link" href="#">
+				<img class="postcard__img" src="https://picsum.photos/500/501" alt="Image Title" />
+			</a>
+			<div class="postcard__text">
+				<h1 class="postcard__title green"><a href="#">Podcast Title</a></h1>
+				<div class="postcard__subtitle small"
+				</div>
+				<div class="postcard__bar"></div>
+				<div class="postcard__preview-txt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, fugiat asperiores inventore beatae accusamus odit minima enim, commodi quia, doloribus eius! Ducimus nemo accusantium maiores velit corrupti tempora reiciendis molestiae repellat vero. Eveniet ipsam adipisci illo iusto quibusdam, sunt neque nulla unde ipsum dolores nobis enim quidem excepturi, illum quos!</div>
+				<ul class="postcard__tagbox">
+					<li class="tag__item"><i class="fas fa-tag mr-2"></i>Podcast</li>
+					<li class="tag__item"><i class="fas fa-clock mr-2"></i>55 mins.</li>
+					<li class="tag__item play green">
+						<a href="#"><i class="fas fa-play mr-2"></i>Play Episode</a>
+					</li>
+				</ul>
+			</div>
+		</article>
+		<article class="postcard dark yellow">
+			<a class="postcard__img_link" href="#">
+				<img class="postcard__img" src="https://picsum.photos/501/501" alt="Image Title" />
+			</a>
+			<div class="postcard__text">
+				<h1 class="postcard__title yellow"><a href="#">Podcast Title</a></h1>
+				<div class="postcard__subtitle small">
+					
+				</div>
+				<div class="postcard__bar"></div>
+				<div class="postcard__preview-txt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, fugiat asperiores inventore beatae accusamus odit minima enim, commodi quia, doloribus eius! Ducimus nemo accusantium maiores velit corrupti tempora reiciendis molestiae repellat vero. Eveniet ipsam adipisci illo iusto quibusdam, sunt neque nulla unde ipsum dolores nobis enim quidem excepturi, illum quos!</div>
+				<ul class="postcard__tagbox">
+					<li class="tag__item"><i class="fas fa-tag mr-2"></i>Podcast</li>
+					<li class="tag__item"><i class="fas fa-clock mr-2"></i>55 mins.</li>
+					<li class="tag__item play yellow">
+						<a href="#"><i class="fas fa-play mr-2"></i>Play Episode</a>
+					</li>
+				</ul>
+			</div>
+		</article>
+	</div>
+        </div>
       </section>
 
 
--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 var animation = lottie.loadAnimation({
     container: document.getElementById('lottie-animation'), // The container element
     renderer: 'html', // Use 'svg', 'canvas', or 'html' as the renderer
-    loop: false, // Set to 'true' if you want the animation to loop
+    loop: true, // Set to 'true' if you want the animation to loop
     autoplay: true, // Set to 'true' if you want the animation to start automatically
     path: 'resources/Animation - 1723227175139.json' // Path to your JSON animation file
 });
 
-//animation.setSpeed(0.5); // Set the playback speed
-//animation.setDirection(1); // 1 for forward, -1 for reverse
+animation.setSpeed(0.5); // Set the playback speed
+animation.setDirection(1); // 1 for forward, -1 for reverse
+
